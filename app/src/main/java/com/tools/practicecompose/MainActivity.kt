c75b4ff27a4fb85c6cba1f1d0d41c15d3f56744e
chris-wang15 <wlw2015@foxmail.com>
2023-05-05 09:11:52 +0200

    flow test for viewmodel

--- app/src/main/java/com/tools/practicecompose/MainActivity.kt
+++ app/src/main/java/com/tools/practicecompose/MainActivity.kt
@@ -3,8 +3,10 @@ package com.tools.practicecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,17 +16,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.tools.practicecompose.ui.theme.PracticeComposeTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+                val viewModelTest : MainViewModel by viewModels()
+//        collectLatestLifecycleFlow(viewModelTest.stateFlow) {
+//            binding.textView.text = it
+//        }
         setContent {
             PracticeComposeTheme {
                 val viewModel = viewModel<MainViewModel>()
-                // TODO wrong way
-                val time = viewModel.countDownFlow.collectAsState(initial = 100)
+                val time = viewModel.countDownFlow.collectAsState(initial = 5)
+                val count = viewModel.stateFlow.collectAsState(initial = 0)
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -36,9 +50,20 @@ class MainActivity : ComponentActivity() {
                             fontSize = 30.sp,
                             modifier = Modifier.align(Alignment.Center),
                         )
+                        Button(onClick = { viewModel.increaseCounter() }) {
+                            Text(text = "Counter: ${count.value}")
+                        }
                     }
                 }
             }
+        }
+    }
+}
+
+fun <T> ComponentActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
         }
     }
 }
--- app/src/main/java/com/tools/practicecompose/MainViewModel.kt
+++ app/src/main/java/com/tools/practicecompose/MainViewModel.kt
@@ -6,10 +6,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
     // 以下是cold flow 没有collect就不会执行；相反还有hot flow
-    val countDownFlow : Flow<Int> = flow {
-        val startingValue = 100
+    val countDownFlow: Flow<Int> = flow {
+        val startingValue = 5
         var curVal = startingValue
         emit(startingValue)
         while (curVal > 0) {
@@ -19,30 +19,127 @@ class MainViewModel: ViewModel() {
         }
     }
 
+    // stateFlow sharedFlow 是 hotFlow
+    private val _stateFlow = MutableStateFlow(0)
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _shareFlow = MutableSharedFlow<Int>()
+    val shareFlow = _shareFlow.asSharedFlow()
+
     init {
-        collectFlow()
+//        collectColdFlow()
+        collectHotFlow()
     }
 
-    private fun collectFlow() {
+    private fun squareNumber(number: Int) {
+        viewModelScope.launch {
+            _shareFlow.emit(number * number)
+        }
+    }
+
+    fun increaseCounter() {
+        _stateFlow.value += 1
+    }
+
+    private fun collectHotFlow() {
+        viewModelScope.launch {
+            shareFlow.collect {
+                delay(2000L)
+                println("FIRST FLOW: received number: $it")
+            }
+        }
+        viewModelScope.launch {
+            shareFlow.collect {
+                delay(3000L)
+                println("SECOND FLOW: received number: $it")
+            }
+        }
+        squareNumber(4)
+    }
+
+    private fun collectColdFlow() {
         // 以下写法等同于viewModelScope.launch {countDownFlow.collect}
 //        countDownFlow.onEach {time->
 //            println("onEach $time")
 //        }.launchIn(viewModelScope)
+
+//        viewModelScope.launch {
+//            countDownFlow
+//                .filter {time->
+//                    time % 2 == 0
+//                }
+//                .map {
+//                    it.toString()
+//                }
+//                .map {
+//                    Integer.valueOf(it)
+//                }
+//                    // 自动取消之前没响应完的，例如println("xx")之前有delay(1500L)
+//                .collectLatest { time->
+//                println("cur time is $time")
+//            }
+//        }
+
+        // count 用法
+//        viewModelScope.launch {
+//            val countVal: Int = countDownFlow
+//                .count {
+//                    it % 2 == 0
+//                }
+//            println("countVal is $countVal")
+//        }
+
+//        viewModelScope.launch {
+//            val reduceResult = countDownFlow.reduce { accumulator: Int, value: Int ->
+//                accumulator + value
+//            }
+//            val foldResult = countDownFlow.fold(100) { accumulator: Int, value: Int ->
+//                accumulator + value
+//            }
+//            println("Results are $reduceResult  and $foldResult")
+//        }
+
+        //flatMapConcat and flatMapLatest
+//        val flow1 = flow {
+//            emit(1)
+//            delay(500L)
+//            emit(2)
+//        }
+//        viewModelScope.launch {
+//            flow1.flatMapConcat {value-> // also flatMapLatest
+//                flow {
+//                    emit(value + 1)
+//                    delay(500L)
+//                    emit(value + 2)
+//                }
+//            }.collect {
+//                println("value is $it")
+//            }
+//        }
+
+        // 利用cold flow, 在collect 中delay emit
+        val flow1 = flow {
+            println("FLOW: start delivering Appetizer")
+            delay(250L)
+            emit("Appetizer")
+            println("FLOW: start delivering Main dish")
+            delay(1000L)
+            emit("Main dish")
+            println("FLOW: start delivering Dessert")
+            delay(100L)
+            emit("Dessert")
+        }
         viewModelScope.launch {
-            countDownFlow
-                .filter {time->
-                    time % 2 == 0
-                }
-                .map {
-                    it.toString()
-                }
-                .map {
-                    Integer.valueOf(it)
-                }
-                    // 自动取消之前没响应完的，例如println("xx")之前有delay(1500L)
-                .collectLatest { time->
-                println("cur time is $time")
+            flow1.onEach {
+                println("FLOW: $it is delivered")
             }
+                // .buffer() // 可以让 collect 不阻塞 emit
+                // .conflate() // 可以让来不及 collect 的忽略掉，不阻塞 emit
+                .collect {
+                    println("FLOW: Now eating $it")
+                    delay(2500L)
+                    println("FLOW: Finished eating $it")
+                }
         }
     }
 }
